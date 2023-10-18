2be1bc7d40d0080b400e096376f022989c71d134
Oscar Castro <oscar.a.castro818@gmail.com>
2023-10-17 23:40:21 -0500

    solved shapeArea ts challenge

--- edgeOfTheOcean.ts
+++ edgeOfTheOcean.ts
@@ -12,3 +12,9 @@ function adjacentElementsProduct(inputArray: number[]): number {
     }
     return Math.max(...productNumbers);
 }
+// Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+//
+//     A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side. You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+function shapeArea(n: number): number {
+    return n*n + (n-1)*(n-1);
+}
