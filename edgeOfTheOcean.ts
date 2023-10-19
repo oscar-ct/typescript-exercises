81d672fdb1e4593c4de15b75b3848d2dd5715a9c
Oscar Castro <oscar.a.castro818@gmail.com>
2023-10-18 23:37:44 -0500

    feat: solve matrix TS challenge

--- edgeOfTheOcean.ts
+++ edgeOfTheOcean.ts
@@ -33,4 +33,33 @@ function makeArrayConsecutive2(statues: number[]): number {
     const high: number = sortedStatues[sortedStatues.length - 1];
     return ((high - low + 1) - (sortedStatues.length));
 }
-
+// Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+//
+//     Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0 < a1 < ... < an. Sequence containing only one element is also considered to be strictly increasing.
+//
+//     Example
+//
+// For sequence = [1, 3, 2, 1], the output should be
+// solution(sequence) = false.
+//
+//     There is no one element in this array that can be removed in order to get a strictly increasing sequence.
+//
+//     For sequence = [1, 3, 2], the output should be
+// solution(sequence) = true.
+//
+//     You can remove 3 from the array to get the strictly increasing sequence [1, 2]. Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
+function almostIncreasingSequence(sequence: number[]): boolean {
+    let count: number = 0;
+    for (let i = 0; i < sequence.length - 1; i++) {
+        if (sequence[i] >= sequence[i+1]) {
+            count++;
+        }
+        if (i - 1 >= 0 && i + 2 <= sequence.length - 1 && sequence[i] - sequence[i+2] >= 0 && sequence[i-1] - sequence[i+1] >= 0) {
+            return false;
+        }
+        if (count > 1) {
+            return false;
+        }
+    }
+    return true;
+}
